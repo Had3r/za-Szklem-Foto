95423b79e252c43204c802cd8537145cab3d0153
Adrian Zawadzki <adrian.zawadzki@outlook.com>
2020-12-24 12:12:20 +0100

    feat: image grid

--- src/components/home-page/FeaturedWorks.js
+++ src/components/home-page/FeaturedWorks.js
@@ -20,6 +20,7 @@ export default () => {
               return (
                 <GridItem key={id}>
                   <Img fluid={image.fluid} />
+                  <Figcaption>{shortDescription}</Figcaption>
                 </GridItem>
               );
             })}
@@ -43,24 +44,63 @@ const Heading = styled.h2`
 `;
 
 const Paragraph = styled.p`
-  ${tw`text-center`}
+  ${tw`text-center mb-10`}
 `;
 
 const PhotographyGrid = styled.div`
   ${tw`container`}
 `;
 
-// TODO: start here from creating custom grid
 const GridRow = styled.div`
+  width: 100%;
   display: grid;
   grid-template-rows: repeat(7, 200px);
-  width: 100%;
+  gap: 20px;
+
+  @media screen and (min-width: 640px) {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: repeat(4, 200px);
+  }
+  @media screen and (min-width: 1024px) {
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(2, 200px);
+    gap: 10px;
+  }
 `;
 
-const GridItem = styled.div`
-  ${tw``};
-  .gatsby-image-wrapper {
-    width: 100%;
-    height: 100%;
+const GridItem = styled.figure`
+  ${tw`relative cursor-pointer overflow-hidden`};
+  &:hover figcaption {
+    ${tw`text-opacity-100`};
+    transform: translate(-50%, -50%);
   }
+  &:hover .gatsby-image-wrapper {
+    transform: scale(1.4);
+  }
+  &:hover .gatsby-image-wrapper img {
+    transform: scale(1.4);
+    filter: blur(1px) brightness(80%);
+  }
+
+  @media screen and (min-width: 1024px) {
+    &:nth-child(3) {
+      grid-area: 1 / 3 / -1 / 4;
+    }
+  }
+  .gatsby-image-wrapper {
+    ${tw`w-full h-full`};
+  }
+  .gatsby-image-wrapper img {
+    /* property override from gatsby-image */
+    transition: 0.5s transform !important;
+  }
+`;
+
+const Figcaption = styled.figcaption`
+  ${tw`absolute text-white uppercase text-center text-base text-opacity-0`};
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, 20%);
+  transition: 0.5s all;
+  backface-visibility: hidden;
 `;
