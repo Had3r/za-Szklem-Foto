8ac21de765898f14b34085393d3e9618f226b15d
Adrian Zawadzki <adrian.zawadzki@outlook.com>
2021-01-06 14:57:29 +0100

    feat [WIP]: creating footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 
 export default () => {
-  return <div>footer</div>;
+  return (
+    <Footer className="long-line">
+      <Wrapper>
+        <Paragraph>Tel: 111 111 111</Paragraph>
+        <Paragraph>Email: email@email.com</Paragraph>
+        <Paragraph>Adres: ul.Dummy 10</Paragraph>
+        <Icons>icons</Icons>
+      </Wrapper>
+    </Footer>
+  );
 };
+
+const Footer = styled.footer`
+  ${tw``};
+  &.long-line::before {
+    ${tw`block`}
+    top: 1rem;
+  }
+`;
+
+const Wrapper = styled.div`
+  ${tw`container py-16 px-4 md:py-20`};
+`;
+
+const Paragraph = styled.p`
+  ${tw`text-sm text-gray-600`};
+`;
+
+// TODO: start here
+const Icons = styled.div`
+  ${tw`mt-8`}
+`;
