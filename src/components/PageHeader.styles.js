2a514bb13cbf87d89d518ea8ec3294ae1e147bae
Adrian Zawadzki <adrian.zawadzki@outlook.com>
2021-01-24 13:46:20 +0100

    refactor: whitespace removed, css format improved

--- src/components/PageHeader.styles.js
+++ src/components/PageHeader.styles.js
@@ -3,14 +3,10 @@ import tw from 'twin.macro';
 
 export const Header = styled.header`
   background: ${props => props.bgColor || '#fff'};
-  &.long-line::before {
-    /* top: ${props => (props.onlySubtitle ? '140px' : '150px')}; */
-  }
 `;
 
 export const Wrapper = styled.div`
   ${tw`container py-12`};
-  /* ${props => props.onlySubtitle && tw`pb-0 -mb-12`} */
 `;
 
 export const Heading = styled.h2`
